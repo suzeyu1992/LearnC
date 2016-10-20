2e1919f9a8f8a58fd1a092938c8fa3b7fb21f707
suzeyu <suzeyu_1992@163.com>
2016-10-20 23:24:49 +0800

    变量的声明(declaration)和定义(difinition)

--- 1_start/OperatePractice.cpp
+++ 1_start/OperatePractice.cpp
@@ -1,4 +1,7 @@
 
+int uninitializedVariableOutside ;
+
+
 int main(){
 
     // 无符号类型相减的时候注意 出现负数的情况
@@ -11,6 +14,28 @@ int main(){
     int i = -42;
     std::cout << i + i << std::endl;    // 输出-84
     std::cout << u + i << std::endl;    // 如果int占32位, 输出4294967264
+
+
+    /******************************字面值常量****************************************/
+    /*
+        十进制 普通数字 字面值默认是十进制带符号数
+        八进制 0开头 字面值可能是无符号或者有符号
+        十六进制 0x开头 字面值可能是无符号或者有符号
+        浮点型 默认为double类型
+    */
+    std::cout << "我是八进制10 输出结果: " <<  010 << std::endl;
+    std::cout << "我是十进制10 输出结果: " <<  10 << std::endl;
+    std::cout << "我是十六进制进制10 输出结果: " <<  0x10 << std::endl;
+
+    /***********************变量*************************************************/
+    std::cout << "\n*************************变量*************************************" << std::endl;
+    int uninitializedVariableInternal ;
+    std::cout << "未被初始化的变量--函数体之内int值:   " << uninitializedVariableInternal << std::endl;
+    std::cout << "未被初始化的变量--函数体之外int值:   " << uninitializedVariableOutside << std::endl;
+
+    extern int declarationInt ;      // 声明一个变量而非定义 , 如果在函数体内部用extern声明的时候试图初始化那么会crash
+    int definitionInt;              // 声明一个变量并定义
+
 
     return 0;
 }
