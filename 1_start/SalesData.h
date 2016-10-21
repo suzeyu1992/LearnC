f918d9be3bde52cc02dcda537fa118daa9f9f225
suzeyu <suzeyu_1992@163.com>
2016-10-22 02:39:06 +0800

    对头文件的一些安全工作处理-- 预处理器使用#define

--- 1_start/SalesData.h
+++ 1_start/SalesData.h
@@ -1,6 +1,12 @@
 
+#ifndef SALES_DATA_H   //判断SALES_DATA_H变量是否未定义, 如果是未定义那么会顺序向下执行, 直到#endif
+#define SALES_DATA_H   // 定义SALES_DATA_H变量
+#include <string>
+
 struct SalesData{
         std::string bookName;
         unsigned sold = 0;
         double revenue = 0.0;
 };
+
+#endif
