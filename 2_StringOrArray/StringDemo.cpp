08232a149a6c7d09000fd246813ca68911bd65a6
suzeyu <suzeyu_1992@163.com>
2016-10-23 00:40:43 +0800

    使用using操作符,  替换标准的::域作用符

---
+++ 2_StringOrArray/StringDemo.cpp
@@ -0,0 +1,14 @@
+// 通过下列using声明, 可以在函数体内直接使用标准库的名字
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <string>
+using std::string
+string s1 = "heihei";
+
+int main(){
+    cout << "使用using声明, 省略标准的 命名空间::变量 的方式" << endl;
+
+
+}
