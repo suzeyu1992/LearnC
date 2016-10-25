38e6e482e10c566e14f8149565150d33f6853236
suzeyu <suzeyu_1992@163.com>
2016-10-25 11:51:28 +0800

    字符串的操作, 转换大小写, 字符串迭代, 字符串下标访问

--- 2_StringOrArray/StringDemo.cpp
+++ 2_StringOrArray/StringDemo.cpp
@@ -44,5 +44,51 @@ int main(){
     // 使用+连接字符串字面值常量和字符串对象的时候要注意,  不允许两个字面值常量字符串进行 + 连接
     // 还有切记字符串字面值与string是不同类型
 
+    cout << "\n******************************开始练习操作字符串*************************************" << endl;
+
+    string operatorStr("some one!!");
+    // 向打印每行输出一个, 最后统计一个字符串中标点符号的个数
+    // 声明一个标记个数的变量 , 变量类型使用decltype()获取和size()返回的类型一样的对象类型
+    decltype(operatorStr.size()) strCount = 0;
+    for(auto c : operatorStr){
+        if (ispunct(c)) {   // 判断单个字符是否是标点
+            ++strCount;
+        }
+        cout << c << endl;
+    }
+    cout << "字符串operator中标点符号个数为: " << strCount  << endl;
+
+
+
+    cout << "\n******************************使用for循环改变字符串中的字符*************************************" << endl;
+    // 转成大写
+    for(auto &c : operatorStr){     // 这里c代表了一个字符引用
+        c = toupper(c);             // 只要把转换后的字符, 赋值给原始字符串中的引用, 即可改变对象
+    }
+
+    cout << "改变后的字符串: "<< operatorStr << endl;
+
+    cout << "\n******************************字符串的下标操作*************************************" << endl;
+    // 替换字符串的最后一个字符为空串
+    if(!operatorStr.empty()){
+        operatorStr[operatorStr.size()-1] = ' ';
+    }
+    cout << "去掉最后一个字符后的字符串: "<< operatorStr << endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
