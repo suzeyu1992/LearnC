b7c4e6fa4200e2fd6a4e525b82ea50a979f18ff1
suzeyu <suzeyu_1992@163.com>
2016-10-30 23:12:27 +0800

    返回*this的成员函数

--- 6_Class/Screen.h
+++ 6_Class/Screen.h
@@ -18,6 +18,11 @@ public:
     char get() const {return contents[cursor];}         // 读取光标处的字符 隐式内联
     inline char get(pos ht, pos wd) const;              // 显示内联
     Screen &move (pos r, pos c);                        // 能在之后被设为内联
+
+    // 添加返回 *this的成员函数
+    Screen &set (char);
+    Screen &set (pos, pos, char);
+
 private:
     pos cursor = 0;
     pos height 0, width = 0;
@@ -36,4 +41,15 @@ Screen &Screen::move(pos r, pos c){
 inline char Screen::get(pos ht, pos wd) const{                  // 在类的内部声明成inline
     pos row = r * width;
     return contents[row + c];   // 返回指定列的字符
+}
+
+inline Screen &Screen::set(char c){
+    contents[cursor] = c;       // 设置当前光标新值
+    return *this;               // 将this对象作为左值返回
+}
+
+inline Screen &Screen::set(pos r, pos col, char ch){
+    contents[r*width + col] = ch;
+    return *this;               // 如果返回的对象的引用, 那么可以直接像builder模式一样链式调用, 并且最主要的区别是
+                                // 非对象引用返回, 返回的只是对象的副本, 通过这个对象的副本修改其值, 并不会对源对象造成影响, 需要区分
 }
